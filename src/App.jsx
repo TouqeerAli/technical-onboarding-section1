be6a9f5fd3156dbcbbb2b5f7128ba1c4670dd149
Touqeer Ali Soomro <engr.touqeerali@gmail.com>
2024-06-02 01:08:20 +0500

    make all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Touqeer Ali",
+  title: "Java Developer",
+  email: "engr.touqeerali@gmail.com",
+  gitHub: "TouqeerAli",
+  instagram: "",
+  linkedIn: "iamtouqeerali",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
